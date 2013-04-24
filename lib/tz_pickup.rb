b34d6df75d9ec6d4b8f6559a50a8528b2e491d0d
Y.Zemlyanukhin <y.zemlyanukhin@mdterra.org>
2013-04-24 14:45:10 +0400

    TzPickup initialized and al tests are passed

--- lib/tz_pickup.rb
+++ lib/tz_pickup.rb
@@ -1,4 +1,5 @@
 require "tz_pickup/version"
+require "kdtree"
 
 module TzPickup
 
@@ -6,6 +7,7 @@ module TzPickup
     DB_PATH = 'db'
     ZONE_FILE = 'zone.tab'
     TREE_FILE = 'zone.tree'  
+    CITY_FILE = 'zone.cities'  
 
     ##
     # Method creates object of TzTree 
@@ -21,12 +23,23 @@ module TzPickup
     ##
     # Method parses +line+ of text
     # to list of three params
+    #
     # Input: line of text
     # Output: list of longitude, latitude 
     # and timezone name
     def self.parse_line(line)
+      rx = %r|[A-Z]{2}\s+(?<latitude>[+\-]\d{4}(?:\d{2})?)(?<longitude>[+\-]\d{5}(?:\d{2})?)\s+(?<timezone>[\w/]+)|
+      matches = line.match(rx)
+      
+      raise ArgumentError, "String should match tz database format (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)" unless matches
+
+      [normalize(matches[:latitude]), normalize(matches[:longitude]), matches[:timezone]]
     end
 
+    def self.normalize(num)
+      return num.to_i * 100 if num =~ /^[+\-]\d{4}\d?$/
+      num.to_i
+    end
     ##
     # Constructor that search for tree-file under +path+
     # and either loads tree to memory 
@@ -36,10 +49,10 @@ module TzPickup
     # Output: TzTree object
     def initialize(path)
       @path = path
-      @tree = if File.exists?(dst_path)
-        load(dst_path)
+      @tree, @cities = if File.exists?(tree_dst_path) && File.exists?(city_dst_path)
+        TzPickup::TzTree.load(tree_dst_path, city_dst_path)
       else
-        create(src_path, dst_path)
+        TzPickup::TzTree.create(src_path, tree_dst_path, city_dst_path)
       end
     end
 
@@ -47,8 +60,12 @@ module TzPickup
       File.join(@path, TzPickup::TzTree::DB_PATH, TzPickup::TzTree::ZONE_FILE)
     end
 
-    def dst_path
+    def tree_dst_path
       File.join(@path, TzPickup::TzTree::DB_PATH, TzPickup::TzTree::TREE_FILE)
+    end
+
+    def city_dst_path
+      File.join(@path, TzPickup::TzTree::DB_PATH, TzPickup::TzTree::CITY_FILE)
     end
 
     ##
@@ -58,30 +75,58 @@ module TzPickup
     # Input: latitude and longitude
     # Output: conventional timezone name
     def find_tz(lat, lon)
-      @tree.nearest(lat, lon)
+      city_index = @tree.nearest(lat, lon)
+      @cities.at(city_index)
     end
 
-    private
     ##
     # Method creates tree structure from +src+ zone-file
-    # and dumps it to +dst+ file
+    # and dumps tree to +tree_dst+ file
+    # and dumps cities list to +city_dst+
     #
     # It raises exception on error of file reading 
     #
     # Input: name of source zone-file, 
-    # name of destination tree file
-    # Output: tree structure for time-zones
-    def create(src, dst)
+    # name of destination tree file, 
+    # name of destination cities file,
+    # Output: list of tree structure for time-zones and cities list 
+    def self.create(src, tree_dst, city_dst)
+      points = []
+      cities = []
 
+      File.open(src, 'r').each do |line|
+        timezone = TzPickup::TzTree.parse_line(line)
+        cities << timezone[2]
+        points << [timezone[0], timezone[1], (cities.size - 1)]
+      end
+
+      tree = Kdtree.new(points)
+
+      File.open(tree_dst, 'w') { |f| tree.persist(f) }
+      File.open(city_dst, 'w') { |f| f.puts cities }
+      
+      [tree, cities]
     end
 
     ##
     # Method loads tree structure
-    # from existing +file+
+    # from existing +zone_file+
+    # and existiong +city_file+
     #
-    # Input: name of source tree file
+    # Input: name of zone tree file
+    # and name of cities file
     # Output: tree structure for time-zones
-    def load(file)
+    # and city list
+    def self.load(zone_file, city_file)
+      cities = []
+
+      tree = File.open(zone_file) { |f| Kdtree.new(f) }
+
+      File.open(city_file, 'r').each do |line|
+        cities << line.chomp
+      end
+
+      [tree, cities]
     end
   end
 
@@ -94,7 +139,7 @@ module TzPickup
   # Input: none
   # Output: root path
   def self.root 
-    (defined?(Rails) && !Rails.root.nil?) ? Rails.root : File.expand_path('../spec')
+    (defined?(Rails) && !Rails.root.nil?) ? Rails.root : File.expand_path('../spec', File.dirname(__FILE__))
   end
 
   ##
@@ -106,6 +151,6 @@ module TzPickup
   # Output: international timezone name
   def self.tz_pickup(lat, lon)
     tree = TzTree.get_tree(root)
-    tree.find_tz(lon, lat)
+    tree.find_tz(lat, lon)
   end
 end
--- spec/tz_pickup_spec.rb
+++ spec/tz_pickup_spec.rb
@@ -5,27 +5,27 @@
 # 3.  Zone name used in value of TZ environment variable.
 
 =begin
-VI  +1821-06456 America/St_Thomas
-VN  +1045+10640 Asia/Ho_Chi_Minh
-VU  -1740+16825 Pacific/Efate
-WF  -1318-17610 Pacific/Wallis
-WS  -1350-17144 Pacific/Apia
-YE  +1245+04512 Asia/Aden
-YT  -1247+04514 Indian/Mayotte
-ZA  -2615+02800 Africa/Johannesburg
-ZM  -1525+02817 Africa/Lusaka
-ZW  -1750+03103 Africa/Harare
+    VI  +1821-06456 America/St_Thomas
+    VN  +1045+10640 Asia/Ho_Chi_Minh
+    VU  -1740+16825 Pacific/Efate
+    WF  -1318-17610 Pacific/Wallis
+    WS  -1350-17144 Pacific/Apia
+    YE  +1245+04512 Asia/Aden
+    YT  -1247+04514 Indian/Mayotte
+    ZA  -2615+02800 Africa/Johannesburg
+    ZM  -1525+02817 Africa/Lusaka
+    ZW  -1750+03103 Africa/Harare
 =end
 require 'tz_pickup'
 require 'mocha/api'
 
 describe TzPickup do
   it "should return the nearest time zone for specified lon and lat" do
-    TzPickup.tz_pickup(-124800, 0451500).should eql 'Indian/Mayotte'
+    TzPickup.tz_pickup(-124800, 451500).should eql 'Indian/Mayotte'
   end
 
   it "should return root path for current env" do
-    TzPickup.root.should eql File.expand_path('../spec')
+    TzPickup.root.should eql File.expand_path(File.dirname(__FILE__))
 
     Rails = mock('Rails')
     Rails.expects(:root).returns('correct rails root path').at_least_once
@@ -37,58 +37,75 @@ end
 describe TzPickup::TzTree do
   before(:all) do
     @src = File.join('spec', TzPickup::TzTree::DB_PATH, TzPickup::TzTree::ZONE_FILE)
-    @dst = File.join('spec', TzPickup::TzTree::DB_PATH, TzPickup::TzTree::TREE_FILE)
+    @zones = File.join('spec', TzPickup::TzTree::DB_PATH, TzPickup::TzTree::TREE_FILE)
+    @cities = File.join('spec', TzPickup::TzTree::DB_PATH, TzPickup::TzTree::CITY_FILE)
     @path = File.expand_path('spec')
   end
 
-  it "should return TzTree object for zone.tab under current root path" do
-    tree_object = TzPickup::TzTree.get_tree(@path)
+  describe "get_tree" do
+    it "should return TzTree object for zone.tab under current root path" do
+      tree_object = TzPickup::TzTree.get_tree(@path)
 
-    tree_object.should respond_to(:find_tz)
+      tree_object.should respond_to(:find_tz)
+    end
   end
 
-  it "should parse line of text with location specified by +-DDMM+-DDDMM to list [latitude, longitude, timezone]" do
-    line = "VI  +1821-06456 America/St_Thomas"
+  describe "parse_line" do
+    it "should parse line of text with location specified by +-DDMM+-DDDMM to list [latitude, longitude, timezone]" do
+      line = "VI  +1821-06456 America/St_Thomas"
 
-    TzPickup::TzTree.parse_line(line).should eql [182100, -0645600, 'America/St_Thomas']
+      TzPickup::TzTree.parse_line(line).should eql [182100, -645600, 'America/St_Thomas']
+    end
+
+    it "should parse line of text with location specified by +-DDMMSS+-DDDMMSS to list [latitude, longitude, timezone]" do
+      line = "US +211825-1575130 Pacific/Honolulu  Hawaii"
+
+      TzPickup::TzTree.parse_line(line).should eql [211825, -1575130, 'Pacific/Honolulu']
+    end
+
+    it "should raise an exception on parsing incorrect line of text" do
+      line = "US 000000-121212121212 some_incorrect_data"
+
+      lambda { TzPickup::TzTree.parse_line(line) }.should raise_error(ArgumentError)
+    end
   end
 
-  it "should parse line of text with location specified by +-DDMMSS+-DDDMMSS to list [latitude, longitude, timezone]" do
-    line = "US +211825-1575130 Pacific/Honolulu  Hawaii"
+  describe "create" do
+    before(:each) do
+      File.delete(@zones) if File.exists?(@zones)
+      File.delete(@cities) if File.exists?(@cities)      
+    end
 
-    TzPickup::TzTree.parse_line(line).should eql [211825, -1575130, 'Pacific/Honolulu']
+    it "should create Kdtree file from zone-file" do
+      tree, cities = TzPickup::TzTree.send(:create, @src, @zones, @cities)
+
+      tree.should respond_to(:nearest)
+      File.exists?(@zones).should be true
+      File.exists?(@cities).should be true
+    end
+
+    it "should raise error if zone-file is unavailable" do   
+      lambda { TzPickup::TzTree.send(:create, 'incorrect/file/name', @zones, @cities) }.should raise_error(Errno::ENOENT)
+    end
   end
 
-  it "should raise an exception on parsing incorrect line of text" do
-    line = "US 000000-121212121212 some_incorrect_data"
+  describe "load" do
+    it "should load Kdtree from zone-file" do
+      TzPickup::TzTree.send(:create, @src, @zones, @cities)
+      tree, cities = TzPickup::TzTree.send(:load, @zones,  @cities)
 
-    lambda { TzPickup::TzTree.parse_line(line) }.should raise_error(ArgumentError)
+      tree.should respond_to(:nearest)
+      cities.should respond_to(:index)
+    end
+
+    it "should raise error if tree-file is unavailable" do
+      lambda { TzPickup::TzTree.send(:load, 'incorrect/file/name', @cities) }.should raise_error(Errno::ENOENT)
+    end
   end
 
-  it "should create Kdtree file from zone-file" do
-    File.delete(@dst) if File.exists?(@dst)
-    tree = TzPickup::TzTree.new(@path).send(:create, @src, @dst)
-
-    tree.should respond_to(:nearest)
-    File.exists?(dst_path).should be true
-  end
-
-  it "should raise error if zone-file is unavailable" do   
-    lambda { TzPickup::TzTree.new(@path).send(:create, 'incorrect/file/name', @dst) }.should raise_error(ArgumentError)
-  end
-
-  it "should load Kdtree from zone-file" do
-    TzPickup::TzTree.new(@path).send(:create, @src, @dst)
-    tree = TzPickup::TzTree.new(@path).send(:load, @dst)
-
-    tree.should respond_to(:nearest)
-  end
-
-  it "should raise error if tree-file is unavailable" do
-    lambda { TzPickup::TzTree.new(@path).send(:load, 'incorrect/file/name') }.should raise_error(ArgumentError)
-  end
-
-  it "should find timezone by given lat and lon" do
-    TzPickup::TzTree.new(@path).find_tz(+182100, -0645600).should eql 'America/St_Thomas'
+  describe "find_tz" do
+    it "should find timezone by given lat and lon" do
+      TzPickup::TzTree.new(@path).find_tz(+182100, -645600).should eql 'America/St_Thomas'
+    end
   end
 end
